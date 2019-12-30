b164303f2ff0d770551f02c8bfff723531f36cc6
Danny van der Meulen <dmeulen@ebay.com>
2019-12-30 23:21:03 +0100

    QnD recovery script fixes

--- recovery.sh
+++ recovery.sh
@@ -11,8 +11,8 @@ pacman --noconfirm -Sy git base-devel python-setuptools
 su - nonroot -c 'git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si --noconfirm; cd -'
 
 su - nonroot -c 'wget https://archive.archlinux.org/packages/l/linux-headers/linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz'
-su - nonroot -c 'sudo pacman -U linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz'
+su - nonroot -c 'sudo pacman --noconfirm -U linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz'
 
-su - nonroot -c 'yay -S zfs-dkms zfs-utils'
+su - nonroot -c 'yay --noconfirm -S zfs-dkms zfs-utils'
 
 modprobe zfs
