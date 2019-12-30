9ae41ac7db600c24a257e3a7316aee8fde5f495d
Danny van der Meulen <dmeulen@ebay.com>
2019-12-30 23:16:18 +0100

    Some fixes

--- recovery.sh
+++ recovery.sh
@@ -8,17 +8,11 @@ echo -en "root ALL=(ALL) ALL\n%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
 
 pacman --noconfirm -Sy git base-devel python-setuptools
 
-su - nonroot
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd -
+su - nonroot -c 'git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si --noconfirm; cd -'
 
-wget https://archive.archlinux.org/packages/l/linux-headers/linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz
-sudo pacman -U linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz
+su - nonroot -c 'wget https://archive.archlinux.org/packages/l/linux-headers/linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz'
+su - nonroot -c 'sudo pacman -U linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz'
 
-yay -S zfs-dkms zfs-utils
-
-exit
+su - nonroot -c 'yay -S zfs-dkms zfs-utils'
 
 modprobe zfs
