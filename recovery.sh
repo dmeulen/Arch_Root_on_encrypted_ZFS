dbcdfdb7424dd70973618e47bd123ae6e7fe7297
Danny van der Meulen <danny@catdevbrain.nl>
2020-07-01 21:13:15 +0200

    update recovery script

--- recovery.sh
+++ recovery.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 mount -o remount,size=4G /run/archiso/cowspace
 
@@ -10,9 +10,23 @@ pacman --noconfirm -Sy git base-devel python-setuptools
 
 su - nonroot -c 'git clone https://aur.archlinux.org/yay.git; cd yay; makepkg -si --noconfirm; cd -'
 
-su - nonroot -c 'wget https://archive.archlinux.org/packages/l/linux-headers/linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz'
-su - nonroot -c 'sudo pacman --noconfirm -U linux-headers-5.3.8.1-1-x86_64.pkg.tar.xz'
+su - nonroot -c 'wget https://archive.archlinux.org/packages/l/linux-headers/linux-headers-$(uname -r | sed 's/-/./')-x86_64.pkg.tar.zst'
+su - nonroot -c 'sudo pacman --noconfirm -U linux-headers-$(uname -r | sed 's/-/./')-x86_64.pkg.tar.zst'
 
 su - nonroot -c 'yay --noconfirm -S zfs-dkms zfs-utils'
 
 modprobe zfs
+
+zpool import -R /mnt -l zroot
+
+cat <<_EOD
+
+# Now do the following
+
+# Mount /boot, change device accordingly
+mount /dev/sdX1 /mnt/boot
+
+# Enter chroot
+arch-chroot /mnt /bin/bash
+
+# Do your fixes
